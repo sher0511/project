4afd932b9185ea3313d747874db2846e9adea8a3
Sherzod <example@gmail.com>
2023-08-25 13:28:04 +0500

    MovieApp

--- Script.js
+++ Script.js
@@ -1,11 +1,19 @@
-"use strict"
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+let a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На сколько оцените его', ''),
+    c = prompt('Один из последних просмотренных фильмов', ''),
+    d = prompt('На сколько оцените его', '');
 
 
-let incr = 10,
-    decr = 10;
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
 
-// ++incr;
-// --decr;
-
-console.log(incr++);
-console.log(decr--);
+    console.log(personalMovieDB);
